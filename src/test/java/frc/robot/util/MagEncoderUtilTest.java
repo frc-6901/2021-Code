9ed8763677fc2dbd1a71aca263c379acad13d192
Matteo <mateus.sakata@gmail.com>
2021-03-29 10:30:31 -0500

    Update formatting

--- src/test/java/frc/robot/util/MagEncoderUtilTest.java
+++ src/test/java/frc/robot/util/MagEncoderUtilTest.java
@@ -8,42 +8,42 @@ import org.junit.Test;
 
 /** Class that tests the system test */
 public class MagEncoderUtilTest {
-    private static final double k6InchWheelMetersPerRotation = 0.47877871986;
+  private static final double k6InchWheelMetersPerRotation = 0.47877871986;
 
-    @Test
-    public void nativeToDistanceTest() {
-        // assert statements
-        Assert.assertEquals(1, MagEncoderUtil.nativeUnitsToDistance(4096, 1), .001);
-        Assert.assertEquals(.1, MagEncoderUtil.nativeUnitsToDistance(4096, 1, 10), .001);
-        Assert.assertEquals(
-                0.478793556,
-                MagEncoderUtil.nativeUnitsToDistance(4096, k6InchWheelMetersPerRotation),
-                .001);
-    }
+  @Test
+  public void nativeToDistanceTest() {
+    // assert statements
+    Assert.assertEquals(1, MagEncoderUtil.nativeUnitsToDistance(4096, 1), .001);
+    Assert.assertEquals(.1, MagEncoderUtil.nativeUnitsToDistance(4096, 1, 10), .001);
+    Assert.assertEquals(
+        0.478793556,
+        MagEncoderUtil.nativeUnitsToDistance(4096, k6InchWheelMetersPerRotation),
+        .001);
+  }
 
-    @Test
-    public void distanceToNative() {
-        Assert.assertEquals(4096, MagEncoderUtil.distanceToNativeUnits(1, 1));
-        Assert.assertEquals(40960, MagEncoderUtil.distanceToNativeUnits(1, 1, 10), .001);
-        Assert.assertEquals(
-                4096,
-                MagEncoderUtil.distanceToNativeUnits(0.478793556, k6InchWheelMetersPerRotation),
-                .001);
-    }
+  @Test
+  public void distanceToNative() {
+    Assert.assertEquals(4096, MagEncoderUtil.distanceToNativeUnits(1, 1));
+    Assert.assertEquals(40960, MagEncoderUtil.distanceToNativeUnits(1, 1, 10), .001);
+    Assert.assertEquals(
+        4096,
+        MagEncoderUtil.distanceToNativeUnits(0.478793556, k6InchWheelMetersPerRotation),
+        .001);
+  }
 
-    @Test
-    public void nativeUnitsToVelocity() {
-        Assert.assertEquals(10, MagEncoderUtil.nativeUnitsToVelocity(4096, 1), .001);
-        Assert.assertEquals(1, MagEncoderUtil.nativeUnitsToVelocity(4096, 1, 10), .001);
-        Assert.assertEquals(
-                4.78793556, MagEncoderUtil.nativeUnitsToVelocity(4096, k6InchWheelMetersPerRotation), .001);
-    }
+  @Test
+  public void nativeUnitsToVelocity() {
+    Assert.assertEquals(10, MagEncoderUtil.nativeUnitsToVelocity(4096, 1), .001);
+    Assert.assertEquals(1, MagEncoderUtil.nativeUnitsToVelocity(4096, 1, 10), .001);
+    Assert.assertEquals(
+        4.78793556, MagEncoderUtil.nativeUnitsToVelocity(4096, k6InchWheelMetersPerRotation), .001);
+  }
 
-    @Test
-    public void velocityToNativeUnits() {
-        Assert.assertEquals(4096, MagEncoderUtil.velocityToNativeUnits(10, 1), .001);
-        Assert.assertEquals(4096, MagEncoderUtil.velocityToNativeUnits(1, 1, 10), .001);
-        Assert.assertEquals(
-                4096, MagEncoderUtil.velocityToNativeUnits(4.78793556, k6InchWheelMetersPerRotation), .001);
-    }
+  @Test
+  public void velocityToNativeUnits() {
+    Assert.assertEquals(4096, MagEncoderUtil.velocityToNativeUnits(10, 1), .001);
+    Assert.assertEquals(4096, MagEncoderUtil.velocityToNativeUnits(1, 1, 10), .001);
+    Assert.assertEquals(
+        4096, MagEncoderUtil.velocityToNativeUnits(4.78793556, k6InchWheelMetersPerRotation), .001);
+  }
 }
